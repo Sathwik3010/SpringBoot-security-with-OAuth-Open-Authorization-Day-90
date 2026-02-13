e31b3544cb85411cc9e70d8cd799b891b2dd03cd
Sathwik3010 <psdev2002@gmail.com>
2026-02-13 20:31:00 +0530

    O auth

---
+++ src/main/java/com/codegnan/controller/MsgRestController.java
@@ -0,0 +1,12 @@
+package com.codegnan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MsgRestController {
+	@GetMapping("/")
+	public String welome() {
+		return "Welome to oracle Corp";
+	}
+}
